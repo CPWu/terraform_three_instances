f64b34519b7195f67a23ea66cda86599b3d38b7b
Chun Wu <the.chun.wu@gmail.com>
2020-11-09 09:39:44 -0500

    Added provider variables & azurerm_resource_group

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+# Provider Variables
+variable "SUBSCRIPTION_ID" {
+    type = string
+}
+variable "CLIENT_ID" {
+    type = string
+}
+variable "CLIENT_SECRET" {
+    type = string
+}
+variable "TENANT_ID" {
+    type = string
+}
+
+# Node Configuration - For Each
 variable "cluster" {
     type = map
     default = {
@@ -11,4 +26,16 @@ variable "cluster" {
 
         }
     }
+}
+
+# Cluster Configuration
+variable "RESOURCE_GROUP_NAME" {
+    type                            = string
+    default                         = "sandbox-rg"
+    description                     = "Name of the resource group where resources will be created"
+}
+variable "AZURE_REGION" {
+    type                            = string
+    default                         = "canadacentral"
+    description                     = "The region the resources will be created in"
 }
