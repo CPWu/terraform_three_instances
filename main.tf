5e0ec2630e20643896b51b0aa59462f9034791e8
Chun Wu <the.chun.wu@gmail.com>
2020-11-09 09:44:23 -0500

    Added Vnet and Subnet configuration

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "kubernetes_rg" {
     name                                = var.RESOURCE_GROUP_NAME
     location                            = var.AZURE_REGION
 }
+
+resource "azurerm_virtual_network" "kubernetes_vnet" {
+    name                                = "${azurerm_resource_group.kubernetes_rg.name}-vnet"
+    location                            = var.AZURE_REGION
+    resource_group_name                 = var.RESOURCE_GROUP_NAME
+    address_space                       = [var.VNET_ADDRESS_SPACE]
+}
+
+resource "azurerm_subnet" "kubernetes_subnet" {
+    name                                = "${var.RESOURCE_GROUP_NAME}-subnet"
+    virtual_network_name                = azurerm_virtual_network.sandbox_vnet.name
+    resource_group_name                 = var.RESOURCE_GROUP_NAME
+    address_prefixes                    = [var.SUBNET_ADDRESS_PREFIX]
+}
--- variables.tf
+++ variables.tf
@@ -39,3 +39,13 @@ variable "AZURE_REGION" {
     default                         = "canadacentral"
     description                     = "The region the resources will be created in"
 }
+variable "VNET_ADDRESS_SPACE" {
+    type                            = string
+    default                         = "10.0.0.0/16"
+    description                     = "Virtual Network address space"
+}
+variable "SUBNET_ADDRESS_PREFIX" {
+    type                            = string
+    default                         = "10.0.2.0/24"
+    description                     = "Subet address space"   
+}
