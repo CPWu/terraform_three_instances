9cd95f1219b92dd339a0813cdf4c4b25acdec8c9
Chun Wu <the.chun.wu@gmail.com>
2020-11-09 10:55:55 -0500

    Test

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,5 @@
 terraform {
     backend "azurerm" {
-        tenant_id                               = var.TENANT_ID
-        subscription_id                         = var.SUBSCRIPTION_ID
         resource_group_name                     = "remote-state"
         storage_account_name                    = "cpwu"
         container_name                          = "tfstate"
